348959221bb7bde723fa2f597b22532cd1014ad9
Kenan Matthew Warren <kmwarren@ncsu.edu>
2015-04-01 16:10:38 -0400

    Adding back in the dialog fix for pre 5.0

--- app/src/main/java/csc495/potato/walk/walkpotato/UI/Dialogs/AppSelectDialog.java
+++ app/src/main/java/csc495/potato/walk/walkpotato/UI/Dialogs/AppSelectDialog.java
@@ -13,10 +13,9 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.app.ProgressDialog;
 import android.widget.ListView;
 import android.widget.Toast;
-
-import com.gc.materialdesign.widgets.ProgressDialog;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -143,8 +142,7 @@ public class AppSelectDialog extends DialogFragment implements
 
         @Override
         protected void onPreExecute() {
-            progress = new ProgressDialog(getActivity(), "Loading app info...",
-                    Color.parseColor("#F38630"));
+            progress = new ProgressDialog(getActivity(), Color.parseColor("#F38630"));
             progress.show();
             super.onPreExecute();
         }
